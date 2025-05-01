052c22e1fdb969ce314c800b8dfa89b4d7e27bf8
Wilmer Fabian Triana <fabiantriana1072@gmail.com>
2025-05-01 11:50:38 -0500

    Se ajustan archivos para tener un solo repositorio completo

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-        //Create DAO
-        ModelLanguageDAO modelLanguageDAO = new ModelLanguageDAO();
-
-        //Create many languages models
-        ModelLanguageAI model1 = new ModelLanguageAI(
-                1,
-                "GPT-4",
-                "Generative",
-                "4.0",
-=======
 public class Main {
     public static void main(String[] args) {
         LanguageModelDAO dao = new LanguageModelDAO();
@@ -22,80 +6,20 @@ public class Main {
                 "GPT-4",
                 "Generative",
                 4,
->>>>>>> 80a42ab207049d71e2a90d1aee3a2d4044eb5ab3
                 1750000,
                 "WebText+Books+Code",
                 9.8
         );
 
-<<<<<<< HEAD
-        ModelLanguageAI model2 = new ModelLanguageAI(
-                2,
-                "BERT",
-                "Bidirectional",
-                "Large",
-=======
         LanguageModel languageModel2 = new LanguageModel(
                 2,
                 "BERT",
                 "Bidirectional",
                 2,
->>>>>>> 80a42ab207049d71e2a90d1aee3a2d4044eb5ab3
                 340000,
                 "Wikipedia+BooksCorpus",
                 8.5
         );
-<<<<<<< HEAD
-
-        ModelLanguageAI model3 = new ModelLanguageAI(
-                3,
-                "LLaMA",
-                "Generative",
-                "2.0",
-                70000,
-                "CommonCrawl+C4",
-                9.2
-        );
-
-        modelLanguageDAO.create(model1);
-        modelLanguageDAO.create(model2);
-        modelLanguageDAO.create(model3);
-
-        //READ - Show all models
-        System.out.println("List of all models created");
-        for (ModelLanguageAI model : modelLanguageDAO.findAll()){
-            System.out.println(model);
-        }
-
-        //READ - Show one model find by ID = 2
-        System.out.println("Find model by ID 2");
-        ModelLanguageAI model = modelLanguageDAO.findById(2);
-        if(model != null){
-            System.out.println(model);
-        }else{
-            System.out.println("Model not found");
-        }
-
-        //UPDATE - Update a model with ID = 3
-        System.out.println("Updating model with ID 3");
-        ModelLanguageAI modelUpdated = new ModelLanguageAI(
-                3,
-                "LLaMA",
-                "Generative",
-                "3.0",
-                120000,
-                "CommonCrawl+C4",
-                9.8
-        );
-
-        if(modelLanguageDAO.update(modelUpdated)){
-            System.out.println("Model successfully updated!");
-            ModelLanguageAI modelUpdatedSuccess = modelLanguageDAO.findById(3);
-            System.out.println(modelUpdatedSuccess);
-        }else{
-            System.out.println("Model not found!");
-        }
-=======
         if(dao.create(languageModel1)){
             System.out.println("Language " + languageModel1.getName() + " created successful");
         }else{
@@ -136,6 +60,6 @@ public class Main {
                     + " - Name Model: " + model.getName());
         }
 
->>>>>>> 80a42ab207049d71e2a90d1aee3a2d4044eb5ab3
     }
+
 }
--- src/ModelAI.java
+++ src/ModelAI.java
@@ -1,18 +1,9 @@
 public class ModelAI {
-<<<<<<< HEAD
-    protected String name;
-    protected String type;
-    protected String version;
-
-    public ModelAI(String name, String type, String version){
-=======
-
     protected String name;
     protected String type;
     protected float version;
 
     public ModelAI(String name, String type, float version){
->>>>>>> 80a42ab207049d71e2a90d1aee3a2d4044eb5ab3
         this.name = name;
         this.type = type;
         this.version = version;
@@ -21,41 +12,6 @@ public class ModelAI {
     public ModelAI(){
         this.name = "";
         this.type = "";
-<<<<<<< HEAD
-        this.version = "1.0";
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    @Override
-    public String toString() {
-        return "Name: " + name
-                + ", Type: " + type
-                + ", Version: " + version;
-    }
-=======
         this.version = 1;
     }
 
@@ -82,7 +38,4 @@ public class ModelAI {
     public float getVersion(){
         return this.version;
     }
-
-
->>>>>>> 80a42ab207049d71e2a90d1aee3a2d4044eb5ab3
 }
